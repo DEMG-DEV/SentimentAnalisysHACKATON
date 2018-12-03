2b6a5afc6852c7e2beb7b31bdae541e766eb39bb
David Mendez Guardado <demg@outlook.com>
2018-12-03 15:57:52 -0600

    -Basic Sentiment analisys.

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# keys and tokens from the Twitter Dev Console
+consumer_key = 'x8w7uaHzfPFv3OQWtG7HmMBEc'
+consumer_secret = 'VVZ3gT7OGzTSwnCSdHhF8mAxAL7oNWQLWHxwea8W6gAxRxOHX5'
+access_token = '956571218429214720-tVL8cd5ujAVgWNIYZea2x8h6Qg3H30W'
+access_token_secret = 'kexvuunFUU6cCGxQlfssIfwiG1iHGWC5OW0qDGE5cXsL5'
